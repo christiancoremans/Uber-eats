2e633c11b4ee6f39fbba84d8576cc9497b0bf25b
christiancoremans <christiancoremans1@hotmail.com>
2025-12-04 13:38:23 +0100

    Laravel project upload

---
+++ openOpdracht/resources/views/auth/register.blade.php
@@ -0,0 +1,22 @@
+<form action="{{ route('register.store')}}" method="POST">
+@csrf
+    <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+    </div>
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+    </div>
+    <div>
+        <label for="password_confirmation">Confirm Password:</label>
+        <input type="password" id="password_confirmation" name="password_confirmation" required>
+    </div>
+    <div>
+        <button type="submit">Register</button>
+    </div>
+</form>
